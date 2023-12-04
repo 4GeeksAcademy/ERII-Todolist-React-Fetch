2c3bb7ffe917ea740fbc760d1c1be28eb18a6b31
Euro Rincon Garrido <134429160+rinconeuro@users.noreply.github.com>
2023-12-04 15:15:27 +0000

    cambios asesoria

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
-//create your first component
 const Home = () => {
+
+	const apiUrl = "https://playground.4geeks.com/apis/fake/todos/user/eurorincon";
+
 	const [inputValue, setInputValue] = useState("");
 	const [todos, setTodos] = useState([])
+
+	const fetchData = async () => {
+		try {
+		  const response = await fetch(apiUrl, {
+			method: "GET",
+			headers: {
+			  "Content-Type": "application/json",
+			},
+		  });
+	
+		  if (response.ok) {
+			const data = await response.json();
+			console.log(data);
+			
+			const loadedData = data.map((e) => e.label);
+			setTodos(...loadedData)
+			console.log(loadedData)
+			console.log(todos)
+			
+			// setSentences([...loadedData]);
+			// console.log(JSON.stringify(loadedData));
+		  }
+		} catch (error) {
+		  console.error(error);
+		}
+	  };
+	  fetchData(); 
+	 
+
 	return (
 		<div className="container">
 			<h1>My Todos</h1>
@@ -21,16 +52,18 @@ const Home = () => {
 					}}
 					placeholder="What do you need to do?"></input>
 				</li>
-				{todos.map((item, index) => (
-					<li>
-						{item}{""}
-						<i className="far fa-times-circle"
-							onClick={() =>
-								setTodos(
-									todos.filter(
-										(t, currentIndex) => index != currentIndex))}></i>
-					</li>
-				))}
+				{todos.map((item, index) => 
+				{
+					return(
+						<li>
+							{item}{""}
+							<i className="far fa-times-circle"
+								onClick={() =>
+									setTodos(
+										todos.filter(
+											(t, currentIndex) => index != currentIndex))}></i>
+						</li>)}
+					)}
 			</ul>
 			<div>{todos.length} tasks</div>
 
