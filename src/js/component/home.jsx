e0c4a2bd90340ce634788eb8cb1476dac7031c61
Euro Rincon Garrido <134429160+rinconeuro@users.noreply.github.com>
2023-12-11 15:58:56 +0000

    fetching

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,34 +8,93 @@ const Home = () => {
 	const [inputValue, setInputValue] = useState("");
 	const [todos, setTodos] = useState([])
 
-	const fetchData = async () => {
+	const createUser = async () =>{
 		try {
-		  const response = await fetch(apiUrl, {
-			method: "GET",
-			headers: {
-			  "Content-Type": "application/json",
-			},
-		  });
-	
-		  if (response.ok) {
-			const data = await response.json();
-			console.log(data);
-			
-			const loadedData = data.map((e) => e.label);
-			setTodos(...loadedData)
-			console.log(loadedData)
-			console.log(todos)
-			
-			// setSentences([...loadedData]);
-			// console.log(JSON.stringify(loadedData));
-		  }
-		} catch (error) {
-		  console.error(error);
-		}
-	  };
-	  fetchData(); 
-	 
+			const response = await fetch(apiUrl, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify ([]),
+			});
 
+			if (response.ok) {
+				const data = await response.json();
+				console.log ('Usuario creado', data);
+			}
+
+			else {console.error('Error al crear el usuario', response.status)}
+		}
+
+		catch (error) {
+			console.error(error);
+		}
+	}
+
+	const getAllTask = async () =>{
+		try {
+			const response = await fetch(apiUrl);
+
+			if (response.ok) {
+				const data = await response.json();
+				console.log (data);
+
+				setTodos(data);
+			
+			}
+
+			else {
+				if (response.status === 404){
+					console.log('Usuario no encontrado');
+					createUser();
+				}
+
+				else {console.error('Error en la solicitud', response.status)}
+			}
+			
+		} catch(error){console.error}
+		
+	}
+
+	const addTask = async (value) =>{
+		try{
+			const newTask = {
+				label: value,
+				done: false
+			};
+
+			const updatedTask = [...todos, newTask];
+
+			const putOptions = {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify (updatedTask),
+			};
+
+			const putResponse = await fetch(apiUrl, putOptions);
+
+			if (putResponse.ok) {
+				setInputValue(""); 
+				getAllTask();
+			}
+
+			else {
+				console.error('Error al agregar la tarea')
+			}
+		}
+
+		catch(error){
+			console.error('Error al agregar la tarea', error)
+		}
+	}
+
+	useEffect(() => {getAllTask()
+		
+
+	  }, []);
+	 
 	return (
 		<div className="container">
 			<h1>My Todos</h1>
@@ -46,22 +105,22 @@ const Home = () => {
 					value={inputValue}
 					onKeyDown={(e) => {
 						if (e.key === "Enter") {
-							setTodos(todos.concat(inputValue));
-							setInputValue("");
+						addTask(inputValue);
 						}
 					}}
 					placeholder="What do you need to do?"></input>
 				</li>
-				{todos.map((item, index) => 
+				{todos.map((item) => 
 				{
 					return(
-						<li>
-							{item}{""}
+						<li key={item.id}>
+							{`${item.label}`}
 							<i className="far fa-times-circle"
 								onClick={() =>
 									setTodos(
-										todos.filter(
-											(t, currentIndex) => index != currentIndex))}></i>
+										)}>
+												
+										</i>
 						</li>)}
 					)}
 			</ul>
